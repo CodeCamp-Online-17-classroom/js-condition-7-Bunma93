0929ad2d9aa2fe1d56627f27de28a6baed3c5563
Bunma93 <bunma93@gmail.com>
2024-06-28 06:32:11 +0700

    Js-condition7

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // write code here
+let num1 = Number(prompt("ค่าที่ 1"));
+let num2 = Number(prompt("ค่าที่ 2"));
+total = num1+num2;
+if (isNaN(num1) || isNaN(num2)) {
+    alert("Invalid number");
+} else {
+    alert(total);
+}
